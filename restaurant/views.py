f2619cc8d2de4a31658f83c849d2a5175429c44e
Gabriel <danquahbadugabriel@gmail.com>
2025-09-26 18:15:31 +0000

    added auth authentication the booking list endpoint, also updated the app's urls.py to obtain auth token for booking view

--- restaurant/views.py
+++ restaurant/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .serializers import MenuSerializer, BookingSerializer
 from .models import Menu, Booking
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 
@@ -21,10 +22,12 @@ class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView
 
 
 class BookingView(generics.ListCreateAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
 
 
 class BookingItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
