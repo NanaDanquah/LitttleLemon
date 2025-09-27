5db1088759ce2e65f10ad312a927c882a0362554
Nana Danquah <125459868+NanaDanquah@users.noreply.github.com>
2025-09-27 19:04:44 +0000

    Updated settings.py

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'LittleLemon',
         'USER': 'root',
-        'PASSWORD': 'Gabriel@20.UG',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         # 'OPTIONS': {
